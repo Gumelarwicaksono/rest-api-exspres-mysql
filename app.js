382d81ff6e1d92edfbaa58f9b000b4bf1d9532f7
gumelar <gumelar.wicaksono6@gmail.com>
2022-11-07 16:04:15 +0700

    create exspres js app

---
+++ app.js
@@ -0,0 +1,23 @@
+const exspress = require('express');
+const app = exspress();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile('./public/index.html', { root: __dirname });
+});
+app.get('/about', (req, res) => {
+  res.sendFile('./public/about.html', { root: __dirname });
+});
+app.get('/contact', (req, res) => {
+  res.sendFile('./public/contact.html', { root: __dirname });
+});
+
+app.use((req, res, next) => {
+  res.send({
+    page: 'Page Not Found',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App runing at : http://localhost:${port}`);
+});
